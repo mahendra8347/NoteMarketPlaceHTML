09dbdb23041b94eabfdee3968a475bc538331385
mahendra parmar <mahendraparmar10999@gmail.com>
2021-03-07 11:20:05 +0530

    Backend Assignment-1 Added

---
+++ MVC/NotesMarketplace/NotesMarketplace/EmailTemplates/ContactUsEmail.cs
@@ -0,0 +1,43 @@
+﻿using NotesMarketplace.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace NotesMarketplace.EmailTemplates
+{
+    public class ContactUsEmail
+    {
+        public static void ContactEmail(ContactUs contactUS)
+        {
+            var fromEmail = new MailAddress("parmarmahendra10999@gmail.com", contactUS.EmailID); //need system email
+            var toEmail = new MailAddress("parmarmahendra10999@gmail.com");
+            var fromEmailPassword = "Mi@2021&"; // Replace with actual password
+            string subject = "" + contactUS.FullName + " - " + contactUS.Subject + " ";
+            string body = "Hello Admin,<br/></br>" + contactUS.Comments + "<br/>";
+            body += "<br/><br/>Regards,<br/>";
+            body += contactUS.FullName;
+
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
+            };
+
+            using (var message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            })
+                smtp.Send(message);
+        }
+    }
+}
---
+++ MVC/NotesMarketplace/NotesMarketplace/Scripts/js/script.js
@@ -0,0 +1,151 @@
+/*==================================================
+					Toggle-Password
+==================================================*/
+
+//For Login and Old password field in Change password page
+
+$("#toggle-password").click(function () {
+
+	var x = document.getElementById("exampleInputPassword1");
+	//    var x = $($(this).attr("toggle"));
+	if (x.type === "password") {
+		x.type = "text";
+	} else {
+		x.type = "password";
+	}
+});
+
+//For New password field in Change password page
+
+$("#toggle-password2").click(function () {
+
+	var x = document.getElementById("exampleInputPassword2");
+	//    var x = $($(this).attr("toggle"));
+	if (x.type === "password") {
+		x.type = "text";
+	} else {
+		x.type = "password";
+	}
+});
+
+//For Confirm password field in Change password page
+
+$("#toggle-password3").click(function () {
+
+	var x = document.getElementById("exampleInputPassword3");
+	//    var x = $($(this).attr("toggle"));
+	if (x.type === "password") {
+		x.type = "text";
+	} else {
+		x.type = "password";
+	}
+});
+
+/*==================================================
+					FAQ
+==================================================*/
+
+//faq toggle stuff
+$(".question-toggle").click(function (e) {
+	e.preventDefault();
+	var notthis = $(".active-tab").not(this);
+	notthis
+		.find(".fa-minus")
+		.addClass("fa-plus")
+		.removeClass("fa-minus");
+	notthis
+		.toggleClass("active-tab")
+		.next(".faqanswer")
+		.slideToggle(300);
+	$(this)
+		.toggleClass("active-tab")
+		.next()
+		.slideToggle("fast");
+	$(this)
+		.find("i")
+		.toggleClass("fa-plus fa-minus");
+});
+
+
+/*==================================================
+					Navbar
+==================================================*/
+$(function () {
+
+	//Show/Hide nav on page load
+	ShowHideNav();
+
+	$(window).scroll(function () {
+
+		//Show/Hide nav on window's Scroll
+		ShowHideNav();
+
+	});
+
+	function ShowHideNav() {
+
+		if ($(window).scrollTop() > 50) {
+
+			// Show white nav
+			$("header").removeClass("navbar-transparent");
+
+			// Show dark logo 
+			$(".navbar-brand-2 img").attr("src", "/Content/Front/images/Login/top-logo_2.png");
+
+		} else {
+
+			// Hide white nav
+			$("header").addClass("navbar-transparent");
+
+			// Show dark logo 
+			$(".navbar-brand-2 img").attr("src", "/Content/Front/images/Login/top-logo.png");
+
+		}
+		if ($(window).width() < 768) {
+			$('header').removeClass('navbar-transparent');
+			// Show dark logo 
+			$(".navbar-brand img").attr("src", "/Content/Front/images/Login/top-logo_2.png");
+		} else {
+			//
+		}
+
+	}
+
+});
+
+
+/*==================================================
+					add and remove active class in navbar
+==================================================*/
+$( '.navbar .navbar-nav a' ).on( 'click', function () {
+	$( '.navbar .navbar-nav' ).find( 'a.active' ).removeClass( 'active' );
+	$( this ).addClass( 'active' );
+});
+
+/*==================================================
+					File input
+==================================================*/
+
+$(".custom-file-input").on("change", function() {
+  var fileName = $(this)
+	.val()
+	.split("\\")
+	.pop();
+  $(this)
+	.siblings(".custom-file-label")
+	.addClass("selected")
+	.html(fileName);
+});
+
+/*==================================================
+					Sell Price input unable and disable
+==================================================*/
+$(document).ready(function () {
+	$('input[name=IsPaid]').click(function () {
+		if ($('input[name=IsPaid]:checked').val() === "false") {
+			document.getElementById('SellingPrice').disabled = true;
+		} else {
+			document.getElementById('SellingPrice').disabled = false;
+		}
+	});
+});
